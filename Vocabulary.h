babba713e093164424ae2bc16e5d086cb4ef0703
Paolo_Estorm <161868629+PaoloEstorm@users.noreply.github.com>
2025-05-06 15:56:38 +0200

    Update 2.14

--- Vocabulary.h
+++ Vocabulary.h
@@ -1,4 +1,3 @@
-#pragma once
 
 // vocabulary
 const char space[] PROGMEM = " ";
@@ -11,8 +10,8 @@ const char arp[] PROGMEM = "ARP";
 const char printmode[] PROGMEM = "MODE";
 const char printlive[] PROGMEM = "LIVE";
 const char length[] PROGMEM = "LENGTH";
-const char select[] PROGMEM = "SELECT";
-const char edit[] PROGMEM = "EDIT";
+const char printselect[] PROGMEM = "SELECT";
+const char editor[] PROGMEM = "EDITOR";
 const char sync[] PROGMEM = "SYNC";
 const char speed[] PROGMEM = "SPEED";
 const char printbpm[] PROGMEM = "BPM";
@@ -35,6 +34,19 @@ const char downcursor[] PROGMEM = "_";
 const char plus[] PROGMEM = "+";
 const char printx[] PROGMEM = "X";
 const char percent[] PROGMEM = "%";
+const char printstop[] PROGMEM = "STOP";
+const char printstart[] PROGMEM = "START";
+const char printtempo[] PROGMEM = "TEMPO";
+const char printrandom[] PROGMEM = "RANDOM";
+const char printtrig[] PROGMEM = "TRIG";
+const char printsnap[] PROGMEM = "SNAP";
+const char printmidi[] PROGMEM = "MIDI*\n\rCHANNEL";
+const char printsend[] PROGMEM = "SEND*";
+const char printext[] PROGMEM = "EXT*";
+const char printmap[] PROGMEM = "MAP*\n\rKEYS";
+const char printsound[] PROGMEM = "SOUND*";
+const char printreboot[] PROGMEM = "REBOOT";
+const char always[] PROGMEM = "ALWAYS";
 
 const char noteNames[][3] PROGMEM = {
   "C", "C#", "D", "D#", "E", "F", "F#", "G", "G#", "A", "A#", "B"
@@ -57,3 +69,23 @@ const char arpmodes[][8] PROGMEM = {
 const char speeds[][5] PROGMEM = {
   "1/32", "1/16", "1/12", "1/8"   
 };
+
+const char modenames[][12] PROGMEM = {
+  " ARP MODE ", " REC MODE ", " SONG MODE ", " LIVE MODE "
+};
+
+const char trigmodes[][7] PROGMEM = {
+  "HOLD", "GATE", "RETRIG"
+};
+
+const char snapmodes[][8] PROGMEM = {
+  "PATTERN", "UP-BEAT", "BEAT"
+};
+
+const char sortmodes[][10] PROGMEM = {
+  "KEY-ORDER", "ORDERED"
+};
+
+const char* const modes[] = { arp, rec, song, printlive };
+
+const char* const sendmodes[] = { off, on, always };
