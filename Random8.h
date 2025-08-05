4c0d9a381b5ee89220166190c813b1aa3b0f9ae8
Paolo_Estorm <161868629+PaoloEstorm@users.noreply.github.com>
2025-08-05 17:46:39 +0200

    Update 2.23
    bugfix

--- Random8.h
+++ Random8.h
@@ -7,7 +7,7 @@ public:
   int8_t get(int8_t min, int8_t max) {
 
     seed = seed * 25173u + 13849u;
-    uint8_t range = uint8_t(max - min);
+    uint8_t range = uint8_t((max + 1) - min);
     uint8_t value = (seed >> 8) & 0xFF;
     uint8_t offset = (uint16_t(value) * range) >> 8;
 
